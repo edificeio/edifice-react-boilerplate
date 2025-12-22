9f3957e9349a58835981aef51c001153ad56be01
RomuC <romuald.coulaud@edifice.io>
2025-12-22 10:32:47 +0100

    feat: #ENABLING-567 redirect old path before rendering (#8)

--- frontend/src/routes/root/index.tsx
+++ frontend/src/routes/root/index.tsx
@@ -1,26 +1,7 @@
 import { Layout, LoadingScreen, useEdificeClient } from '@edifice.io/react';
 
-import { matchPath } from 'react-router-dom';
-
-import { basename } from '..';
-
 /** Check old format URL and redirect if needed */
 export const loader = async () => {
-  const hashLocation = location.hash.substring(1);
-
-  // Check if the URL is an old format (angular root with hash) and redirect to the new format
-  if (hashLocation) {
-    const isPath = matchPath('/view/:id', hashLocation);
-
-    if (isPath) {
-      // Redirect to the new format
-      const redirectPath = `/id/${isPath?.params.id}`;
-      location.replace(
-        location.origin + basename.replace(/\/$/g, '') + redirectPath,
-      );
-    }
-  }
-
   return null;
 };
 
